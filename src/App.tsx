be3a63fe411dd946206492e0224653af3d43ba33
YSRKEN <ysr.ken@gmail.com>
2020-03-01 19:40:49 +0900

    [add] BootstrapとReact-Bootstrapを導入した

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import 'App.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="my-3">
+        <Col className="text-center">
+          <h1>EV変換機</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
